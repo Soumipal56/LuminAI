bde928d54ae116f4b435b40ede2597776cc1f04a
Soumipal56 <soumiisc2020@gmail.com>
2026-03-12 08:19:43 +0530

    nodemailer done

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cookieParser from "cookie-parser"
+import authRouter from "./routes/auth.routes.js"
 
 const app = express()
 
@@ -8,5 +9,8 @@ app.use(express.json())
 
 // third-party middleware
 app.use(cookieParser())
+
+// routes
+app.use("/api/auth", authRouter)
 
 export default app
